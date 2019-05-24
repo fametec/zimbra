b240273e184216af5a740e5002855c7cc46940ee
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2019-05-24 09:56:13 -0300

    Create restore_account.sh

---
+++ tools/backup/restore_account.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#set -xv
+
+if [ -z $1 ]
+then
+    echo "First param not found..."
+    echo "Use: $0 account backup_file.tgz"
+    exit 1
+ fi
+
+if [ -z $2 ]
+then
+    echo "Second params not found..."
+    echo "Use: $0 account backup_file.tgz"
+    exit 1
+fi
+
+
+if [ "$USER" != "zimbra" ]
+then
+    echo "Is not zimbra user"
+    echo "use: su - zimbra"
+    exit 2
+fi
+
+
+echo -n "Restoring ${1}..."
+
+/opt/zimbra/bin/zmmailbox -z -m "$1" pru '//?fmt=tgz&resolve=reset' "$2"
+
+if [ $? -eq 0 ]
+then
+
+    echo "done"
+
+else
+
+    echo "fail"
+    exit 3
+fi
+
+
+
+
