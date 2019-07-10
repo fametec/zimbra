d496d1f010cb13b23b5ad3fb62fae42eefbc377c
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2019-07-10 14:32:15 -0300

    Update backupLdap.sh

--- tools/backup/backupLdap.sh
+++ tools/backup/backupLdap.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
-/opt/zimbra/libexec/zmslapcat /opt/zimbra/backup/ldap
-/opt/zimbra/libexec/zmslapcat -c /opt/zimbra/backup/ldap
-/opt/zimbra/libexec/zmslapcat -a /opt/zimbra/backup/ldap
+
+
+set -xv
+
+NOW=`date +%Y_%m_%d_%H_%M`
+
+
+BACKUP_DIR=/opt/zimbra/backup/ldap
+NFS_DIR=/backup-zimbra/ldap
+
+if [ -d $BACKUP_DIR ]
+then
+        mkdir -p $BACKUP_DIR
+        chown zimbra:zimbra $BACKUP_DIR
+fi
+
+
+if [ -d $NFS_DIR/$NOW ]
+then
+        mkdir -p $NFS_DIR/$NOW/
+fi
+
+
+
+su - zimbra -c "/opt/zimbra/libexec/zmslapcat $BACKUP_DIR"
+su - zimbra -c "/opt/zimbra/libexec/zmslapcat -c $BACKUP_DIR"
+su - zimbra -c "/opt/zimbra/libexec/zmslapcat -a $BACKUP_DIR"
+
+
+mv $BACKUP_DIR $NFS_DIR/$NOW/
+
+
