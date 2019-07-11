caafb7a6ea9a6b267a035d1f973b3f6210e76799
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2019-07-11 08:06:43 -0300

    Create backup_configs.sh

---
+++ tools/backup/backup_configs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+DESTINATION=/opt/zimbra/backup/config/
+
+NOW=`date +%Y%m%d%H%M`
+
+
+if [ -d $DESTINATION ]
+then
+
+        mkdir -p $DESTINATION
+
+        chown zimbra:zimbra $DESTINATION
+
+fi
+
+
+cd /opt/zimbra
+
+tar -zcvf $DESTINATION/configs-$NOW.tar.gz \
+        config.* \
+        conf \
+        common \
+        backup/scripts \
+        ssl \
+        zimlets
+
+
+if [ $? -eq 0 ]
+then
+
+        echo "done"
+
+else
+
+        echo "fail"
+        exit 1
+
+fi
+
+
