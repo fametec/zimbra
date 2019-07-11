69ab591405fdee991da89e0b663e4945985ccc0c
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2019-07-11 07:42:02 -0300

    Create backup_with_report.sh

---
+++ tools/backup/backup_with_report.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+## DEBUG
+# set -x
+
+# Require: backup_account.sh
+
+
+OUTPUT=/opt/zimbra/backup/relatorio_backup.csv
+
+if [ -z $1 ]; then
+        ACCOUNTS="`su - zimbra -c "zmprov -l gaa" | sort`"
+else
+        ACCOUNTS=$1
+fi
+
+
+if [ -x backup_account.sh ]
+then
+
+  echo "Error: backup_account.sh not found. "
+
+fi
+
+
+echo '"start_date","account","mailbox_size","backup_size","end_date"' > $OUTPUT
+
+for i in $ACCOUNTS
+
+do
+
+  MBOXSIZE=`su - zimbra -c "zmmailbox -z -m $i gms"`
+
+  echo -n "\"`date +%Y-%m-%d\ %H:%M`\"," >> $OUTPUT
+
+  echo -n "\"$i\"," >> $OUTPUT
+
+  ./backup_account.sh $i
+
+  echo -n "\"$MBOXSIZE\"," >> $OUTPUT
+
+  echo -n "\"`ls -lh /backup-zimbra/accounts/${i}.tgz | cut -d ' ' -f 5` \"," >> $OUTPUT
+
+  echo "\"`date +%Y-%m-%d\ %H:%M`\"" >> $OUTPUT
+
+  tail -n 1 $OUTPUT
+
+done
+
+##
+
+cat $OUTPUT
