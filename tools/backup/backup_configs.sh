27d430a9ea9fe9f6314d0ae4f35ac3ab0b77cdb7
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2019-07-11 08:32:18 -0300

    Update backup_configs.sh

--- tools/backup/backup_configs.sh
+++ tools/backup/backup_configs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+## Debug
+# set -xv
+#
+
+
 DESTINATION=/opt/zimbra/backup/config/
 
 NOW=`date +%Y%m%d%H%M`
@@ -14,16 +19,22 @@ then
 
 fi
 
+TEMPDIR=`mktemp -d`
+
+echo -n "Backup configs to $DESTINATION/configs-$NOW.tar.gz ... " 
+
+su - zimbra -c "zmlocalconfig --show" > $TEMPDIR/zmlocalconfig.txt
 
 cd /opt/zimbra
 
-tar -zcvf $DESTINATION/configs-$NOW.tar.gz \
+tar -zcf $DESTINATION/configs-$NOW.tar.gz \
         config.* \
         conf \
-        common \
+        common/conf \
         backup/scripts \
         ssl \
-        zimlets
+        zimlets \
+        $TEMPDIR/zmlocalconfig.txt
 
 
 if [ $? -eq 0 ]
@@ -38,4 +49,4 @@ else
 
 fi
 
-
+rm -rf $TEMPDIR
