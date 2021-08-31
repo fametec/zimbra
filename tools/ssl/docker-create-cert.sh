afc4825e56448cbd665e3d2d49f51ffb8e750eeb
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2021-08-31 10:41:25 -0300

    Create docker-create-cert.sh

---
+++ tools/ssl/docker-create-cert.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+su – zimbra -c 'zmproxyctl stop'
+#
+docker run -it --rm \
+-p 80:80 \
+-p 443:443 \
+-v $(pwd):/etc/letsencrypt \
+certbot/certbot \
+certonly --standalone \
+--email suporte@fametec.com.br --agree-tos \
+-d mail.poc.eftech.com.br \
+-d smtp.poc.eftech.com.br \
+-d zimbra.poc.eftech.com.br \
+-d webmail.poc.eftech.com.br \
+-d pop.poc.eftech.com.br \
+-d imap.poc.eftech.com.br \
+--no-eff-email
+#
+su – zimbra -c 'zmproxyctl start'
