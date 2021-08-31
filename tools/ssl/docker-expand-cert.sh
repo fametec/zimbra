2bffd2c60d64f9cb4ce50f9d81f31b3da7bb5007
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2021-08-31 10:43:18 -0300

    Create docker-expand-cert.sh

---
+++ tools/ssl/docker-expand-cert.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+su – zimbra -c 'zmproxyctl stop'
+#
+docker run -it --rm \
+-p 80:80 \
+-p 443:443 \
+-v $(pwd):/etc/letsencrypt \
+certbot/certbot \
+certonly --standalone --expand \
+-d mail.poc.eftech.com.br \
+-d novo.poc.eftech.com.br
+#
+su – zimbra -c 'zmproxyctl start'
