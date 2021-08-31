5c33c60d292aa281bd07e6c77243161d5faba7f1
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2021-08-31 10:42:40 -0300

    Update docker-renew-cert.sh

--- tools/ssl/docker-renew-cert.sh
+++ tools/ssl/docker-renew-cert.sh
@@ -10,21 +10,3 @@ certbot/certbot \
 --standalone renew
 #
 su – zimbra -c 'zmproxyctl start'
-EOF
-
-
-cat <<EOF > expand-cert.sh 
-#!/bin/bash
-#
-su – zimbra -c 'zmproxyctl stop'
-#
-docker run -it --rm \
--p 80:80 \
--p 443:443 \
--v $(pwd):/etc/letsencrypt \
-certbot/certbot \
-certonly --standalone --expand \
--d mail.poc.eftech.com.br \
--d novo.poc.eftech.com.br
-#
-su – zimbra -c 'zmproxyctl start'
