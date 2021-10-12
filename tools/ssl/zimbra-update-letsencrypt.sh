581870d3dd616ef13230751f5f68f7d5fd7dc57f
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2021-10-12 18:49:54 -0300

    Update zimbra-update-letsencrypt.sh

--- tools/ssl/zimbra-update-letsencrypt.sh
+++ tools/ssl/zimbra-update-letsencrypt.sh
@@ -12,16 +12,15 @@ fi
 
   cp -f -a /opt/zimbra/ssl/letsencrypt /opt/zimbra/backup/ssl/letsencrypt/letsencrypt.$(date "+%Y%m%d%H%M") || exit 1
   
-  certbot renew --pre-hook "su - zimbra -c 'zmproxyctl stop'" --post-hook "su - zimbra -c 'zmproxyctl start'" || exit 2
+  certbot --preferred-chain  "ISRG Root X1" renew --pre-hook "su - zimbra -c 'zmproxyctl stop'" --post-hook "su - zimbra -c 'zmproxyctl start'" || exit 2
   
   cd /opt/zimbra/ssl/letsencrypt
   
-  cat /etc/letsencrypt/live/${DOMAIN}/chain.pem /root/dst_root_ca_x3.txt > /root/chain-ca.pem 
-
-  cat /root/chain-ca.pem > /etc/letsencrypt/live/${DOMAIN}/chain.pem
+  cat isrgrootx1.pem lets-encrypt-r3.pem /etc/letsencrypt/live/${DOMAIN}/chain.pem > chain-ca.pem 
 
   cp -f /etc/letsencrypt/live/${DOMAIN}/* .
   
+  chown zimbra:zimbra /opt/zimbra/ssl/letsencrypt
 
 su - zimbra -c '/opt/scripts/deploy-zimbra-letsencrypt.sh' 
 
